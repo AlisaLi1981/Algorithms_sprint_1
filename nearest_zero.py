3fcee40502447dc2a9f6d8a9c054999bd53d849e
AlisaLi1981 <124600383+AlisaLi1981@users.noreply.github.com>
2023-09-24 16:54:53 +0300

    Update nearest_zero.py

--- nearest_zero.py
+++ nearest_zero.py
@@ -1,50 +1,29 @@
-# ID 91191536
+# ID 91235448
 
-def count_zero_distance(street_numbers):
-    n = len(street_numbers)
-    zero_distance = [0] * n  # Список расстояний доближайшего нуля
+from typing import Optional
 
-    nearest_zero = float('-inf') # Переменная для отслеживания индекса ближайшего нуля
-    # Проход в цикле слева направо для получения расстояния до ближайшего нуля слева.
-    for i in range(n):
-        if street_numbers[i] == '0':
-            nearest_zero = i # Если текущий элемент 0, обновляем переменную nearest_zero.
-        zero_distance[i] = i - nearest_zero # Вычисление расстояния до ближайшего нуля слева.
 
-    nearest_zero = float('inf') # Переменная для отслеживания индекса ближайшего нуля
-    # Проход в цикле справа налево для получения расстояния до ближайшего нуля справа.
-    for i in range(n - 1, -1, -1):
-        if street_numbers[i] == '0':
-            nearest_zero = i  # Если текущий элемент 0, обновляем переменную nearest_zero.
-        zero_distance[i] = min(zero_distance[i], nearest_zero - i) # Вычисление расстояния до ближайшего нуля справа.
+def count_zero_distance(n, street_numbers):
+    zero_distance = [None] * n
+    nearest_zero: Optional[int] = None
+
+    for i, number in enumerate(street_numbers):
+        if number == 0:
+            nearest_zero = i
+        if nearest_zero is not None:
+            zero_distance[i] = i - nearest_zero
+
+    nearest_zero = None
+    for i, number in reversed(list(enumerate(street_numbers))):
+        if number == 0:
+            nearest_zero = i
+        if nearest_zero is not None:
+            if zero_distance[i] is None or nearest_zero - i < zero_distance[i]:
+                zero_distance[i] = nearest_zero - i
 
     return zero_distance
 
 if __name__ == '__main__':
-    input()
-    print(*count_zero_distance(input().split()))
-
-# Изначальный вариант и его модификации содержали вложеный цикл, из-за чего превышали лимит времени, хоть и решали задачу:
-# def count_zero_distance(street_numbers):
-#    zero_distance = []
-#    zeros = []
-#    n = len(street_numbers)
-#
-#    for position, number in enumerate(street_numbers):
-#        if number == '0':
-#            zeros.append(position)
-#
-#    for i in range(n):
-#        min_distance = n
-#        for j in zeros:
-#            print(min_distance)
-#            distance = abs(i - j)
-#            if distance < min_distance:
-#                min_distance = distance
-#        zero_distance.append(min_distance)
-#
-#    return zero_distance
-
-# if __name__ == '__main__':
-#    input()
-#    print(*count_zero_distance(input().split()))
+    n = int(input())
+    street_numbers = [int(x) for x in input().split()]
+    print(*count_zero_distance(n, street_numbers))
